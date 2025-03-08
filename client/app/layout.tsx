0443565d6e37b199343a7e5c5c340fae1410eb91
Apollosamaa <apollosamadesu@gmail.com>
2025-03-09 00:52:34 +0800

    changed font to roboto

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import {Roboto } from "next/font/google"
 import "./globals.css";
 import ContextProvider from "@/providers/ContextProvider";
+import { Toaster } from "react-hot-toast";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const roboto = Roboto({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["400", "500", "700", "900"],
 });
 
 export const metadata: Metadata = {
@@ -26,8 +22,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${roboto.className} antialiased`}
       >
+        <Toaster position="top-center"/>
         <ContextProvider>{children}</ContextProvider>
       </body>
     </html>
