0a9783b8e9e388db538d7f1c80372e5ae440c1b8
Apollosamaa <apollosamadesu@gmail.com>
2025-03-31 04:43:58 +0800

    added image domain allow for gravatar and also cdn.auth0 (default auth)

--- client/next.config.ts
+++ client/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: false, //on debug mode only
   images: {
-    domains: ["lh3.googleusercontent.com"],
+    domains: ["lh3.googleusercontent.com", "s.gravatar.com", "cdn.auth0.com"],
 
   }
 };
